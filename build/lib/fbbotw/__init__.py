47788f7643346f965b6e5d946330fb74c9d74d37
Sadman Hasan'git config --global user.name Sadman <sadmanhassan08@gmail.com'git config --global user.email sadmanhassan08@gmail.comgit config --global user.email "sadmanhassan08@gmail.com>
2019-07-11 12:13:06 +0600

    Updated importing in init.py

--- build/lib/fbbotw/__init__.py
+++ build/lib/fbbotw/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from .fbbotw import *
 
 __author__ = """Joabe Mendes"""
 __email__ = 'joabe.mdl@gmail.com'
 __version__ = '2.1.3'
+
--- fbbotw/__init__.py
+++ fbbotw/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from .fbbotw import *
 
 __author__ = """Joabe Mendes"""
 __email__ = 'joabe.mdl@gmail.com'
